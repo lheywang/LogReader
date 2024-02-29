93e4b99077eb2c72f532cef63acfc8d246114c92
lheywang <90984325+lheywang@users.noreply.github.com>
2024-02-29 22:05:47 +0100

    Read first files and started finding informations

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import os
+
 print("Hello World !")
+
+f = []
+layer = 1
+w = os.walk("/")
+for (dirpath, dirnames, filenames) in w:
+    if layer == 2:
+        f.extend(dirnames)
+        break
+    layer += 1
+
+print(f)
