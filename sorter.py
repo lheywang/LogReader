3e6543ba56a0e5fe5af02f52ce7e7f1fc1865c62
lheywang <90984325+lheywang@users.noreply.github.com>
2024-03-09 11:09:54 +0100

    Corrected and Added Treatement engine that sort the logs, remove non human caracters

--- sorter.py
+++ sorter.py
@@ -9,6 +9,36 @@ def SeekOnList(msg: str, words: list):
     return False
 
 
+def RemoveUselessCharacters(msg):
+    output = ""
+    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 "
+    s_msg = str(msg)
+    for strc in s_msg.split("\\x"):
+        if strc != "":
+            if len(strc) > 6:
+                for letter in strc:
+                    out = strc
+                    if letter in letters:
+                        out = out + letter
+
+                if out.isascii():
+                    output = output + out
+    output = output[:-1]
+    for ch in output:
+        if ch.isnumeric():
+            output = output[1:]
+        else:
+            break
+    return output
+
+
+def ClearLogEntry(Log: LogEntry):
+    Log.msg = RemoveUselessCharacters(Log.msg)
+    Log.src = RemoveUselessCharacters(Log.src)
+    Log.date = RemoveUselessCharacters(Log.date)
+    return Log
+
+
 def sorter(logs: list, Levels: dict):
     # Creating the output container
     LevelsID = []
@@ -20,6 +50,8 @@ def sorter(logs: list, Levels: dict):
         LogLevel.append([])
 
     for log in logs:
+        # Remove usuless data (Non ascii characters from the string)
+        log = ClearLogEntry(log)
         for Level, WordList in Levels.items():
             # Now create multiples levels of logs...
             result = SeekOnList(log.msg, WordList)
